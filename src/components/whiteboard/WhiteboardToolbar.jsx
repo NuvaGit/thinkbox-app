f16a7f2d3e04036dfd2b6c831afcae3ab15c6169
NuvaGit <jack.neilan@ucdconnect.ie>
2025-03-17 17:32:55 +0000

    t

--- src/components/whiteboard/WhiteboardToolbar.jsx
+++ src/components/whiteboard/WhiteboardToolbar.jsx
@@ -42,6 +42,7 @@ const WhiteboardToolbar = ({
     { id: 'pan', icon: <Hand size={20} />, label: 'Pan' },
   ];
   
+  
   const predefinedColors = [
     '#000000', // Black
     '#ffffff', // White
